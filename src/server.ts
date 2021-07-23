71d258c8e3396340c57ec7084f9c9f51ccd581c6
Enzo Gab <enzovieira16@hotmail.com>
2021-07-23 10:52:23 -0300

    Criando a documentação de criação de categoria

--- src/server.ts
+++ src/server.ts
@@ -13,4 +13,6 @@ server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 server.use(router)
 
+server.get("/", (_, res) => res.send("Api funcionando"))
+
 server.listen(3333, () => console.log("API online!"))
