c230ca2e95e9d70777363b280f6475b3cee4c423
Enzo Gab <enzovieira16@hotmail.com>
2021-08-16 19:04:11 -0300

    Criando migration de especificação

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { getRepository, Repository } from "typeorm"
+
 import { Specification } from "../../entities/Specification"
 import {
   ICreateSpecificationDTO,
@@ -5,34 +7,25 @@ import {
 } from "../ISpecificationsRepository"
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[]
-
-  private static INSTANCE: SpecificationsRepository
+  private repository: Repository<Specification>
 
   constructor() {
-    this.specifications = []
+    this.repository = getRepository(Specification)
   }
 
-  public static getInstance(): SpecificationsRepository {
-    if (!SpecificationsRepository.INSTANCE) {
-      SpecificationsRepository.INSTANCE = new SpecificationsRepository()
-    }
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
+      description,
+      name,
+    })
 
-    return SpecificationsRepository.INSTANCE
+    await this.repository.save(specification)
   }
 
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification()
-
-    Object.assign(specification, { name, description, createdAt: new Date() })
-
-    this.specifications.push(specification)
-  }
-
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(
-      specification => specification.name === name
-    )
+  async findByName(name: string): Promise<Specification> {
+    const specification = this.repository.findOne({
+      name,
+    })
 
     return specification
   }
