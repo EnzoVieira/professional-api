8bcb3d3d5fe1630c3989a18fd6ddce9d734be605
Enzo Gab <enzovieira16@hotmail.com>
2021-08-20 13:25:56 -0300

    criando teste de criação de categoria

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../entities/Category"
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository"
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const newCategory = new Category()
+
+    Object.assign(newCategory, { name, description })
+
+    this.categories.push(newCategory)
+  }
+
+  async list(): Promise<Category[]> {
+    const list = this.categories
+
+    return list
+  }
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find(category => category.name === name)
+
+    return category
+  }
+}
+
+export { CategoriesRepositoryInMemory }
